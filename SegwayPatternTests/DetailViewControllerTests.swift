91109a437be12ac18b247ea56254c29f494c384e
Geektree0101 <h2s1880@gmail.com>
2021-11-20 22:28:07 +0900

    add createViewController

--- SegwayPatternTests/DetailViewControllerTests.swift
+++ SegwayPatternTests/DetailViewControllerTests.swift
@@ -11,6 +11,12 @@ import XCTest
 
 class DetailViewControllerTests: XCTestCase {
 
+  private func createViewController(articleID: Int = -1) -> DetailViewController {
+    let viewController = DetailViewController(articleID: articleID)
+    // TODO:
+    return viewController
+  }
+
   override func setUpWithError() throws {
     // Put setup code here. This method is called before the invocation of each test method in the class.
   }
